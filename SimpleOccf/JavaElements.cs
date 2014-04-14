0657aa8c0734e5f2c210a062ff203e1565a3876a
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-04-14 11:05:13 +0900

    Add initial files.

---
+++ SimpleOccf/JavaElements.cs
@@ -0,0 +1,64 @@
+﻿#region License
+
+// Copyright (C) 2009-2012 Kazunori Sakamoto
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using Code2Xml.Core.Generators;
+
+namespace SimpleOccf {
+    public static class JavaElements {
+        public static IEnumerable<CstNode> Statement(CstNode root) {
+            return root.Descendants("statement");
+        }
+
+        public static IEnumerable<CstNode> If(CstNode root) {
+            return root.Descendants("statement")
+                    .Where(e => e.FirstChild.TokenText == "if");
+        }
+
+        public static IEnumerable<CstNode> IfAndElseProcesses(CstNode root) {
+            return root.Elements("statement");
+        }
+
+        public static IEnumerable<CstNode> While(CstNode root) {
+            return root.Descendants("statement")
+                    .Where(e => e.FirstChild.TokenText == "while");
+        }
+
+        public static CstNode WhileProcess(CstNode element) {
+            return element.Children().ElementAt(2);
+        }
+
+        public static IEnumerable<CstNode> DoWhile(CstNode root) {
+            return root.Descendants("statement")
+                    .Where(e => e.FirstChild.TokenText == "do");
+        }
+
+        public static CstNode DoWhileProcess(CstNode element) {
+            return element.Children().ElementAt(1);
+        }
+
+        public static IEnumerable<CstNode> For(CstNode root) {
+            return root.Descendants("forstatement");
+        }
+
+        public static CstNode ForProcess(CstNode element) {
+            return element.Child("statement");
+        }
+    }
+}
---
+++ SimpleOccf/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Code2Xml.Core.Generators;
+
+namespace SimpleOccf {
+    internal class Program {
+        private const string BackupExtension = ".soccf_backup";
+
+        private static void Main(string[] args) {
+            var iStatement = 0;
+            var paths = args.SelectMany(arg => {
+                if (Directory.Exists(arg)) {
+                    return Directory.GetFiles(arg, "*.java", SearchOption.AllDirectories);
+                } else if (File.Exists(arg)) {
+                    return Enumerable.Repeat(arg, 1);
+                } else {
+                    return Enumerable.Repeat(arg, 0);
+                }
+            }).Select(Path.GetFileName).Distinct();
+
+            foreach (var path in paths) {
+                var backupPath = path + BackupExtension;
+                if (!File.Exists(backupPath)) {
+                    File.Copy(path, backupPath);
+                }
+                var gen = CstGenerators.JavaUsingAntlr3;
+                var tree = gen.GenerateTreeFromCodePath(backupPath);
+
+                foreach (var node in FindLackingBlockNodes(tree)) {
+                    node.InsertCodeBeforeSelf("{");
+                    node.InsertCodeAfterSelf("}");
+                }
+
+                foreach (var stmt in JavaElements.Statement(tree)) {
+                    stmt.InsertCodeAfterSelf("randoop.multi.OCCF.stmt(" + iStatement + ");");
+                    iStatement++;
+                }
+
+                gen.GenerateTreeFromCodeText(tree.Code, true);
+
+                File.WriteAllText(path, tree.Code);
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            Console.WriteLine(iStatement);
+        }
+
+        private static IEnumerable<CstNode> FindLackingBlockNodes(CstNode root) {
+            var ifs = JavaElements.If(root)
+                    .SelectMany(JavaElements.IfAndElseProcesses);
+            var whiles = JavaElements.While(root)
+                    .Select(JavaElements.WhileProcess);
+            var dos = JavaElements.DoWhile(root)
+                    .Select(JavaElements.DoWhileProcess);
+            var fors = JavaElements.For(root)
+                    .Select(JavaElements.ForProcess);
+
+            return ifs.Concat(whiles)
+                    .Concat(dos)
+                    .Concat(fors);
+        }
+    }
+}
