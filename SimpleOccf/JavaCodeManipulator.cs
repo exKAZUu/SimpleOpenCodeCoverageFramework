fa7c734f5f63138d9a05c811b17d47edb8475c48
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-04-14 18:08:49 +0900

    Dealing with while(true).

--- SimpleOccf/JavaCodeManipulator.cs
+++ SimpleOccf/JavaCodeManipulator.cs
@@ -17,7 +17,7 @@ namespace SimpleOccf {
         }
 
         public void ModifyStatements(CstNode root) {
-            foreach (var stmt in JavaElements.Statement(root)) {
+            foreach (var stmt in FindStatementNodes(root)) {
                 stmt.InsertCodeBeforeSelf("randoop.multi.OCCF.stmt(" + StatementCount + ");");
                 StatementCount++;
             }
@@ -25,9 +25,11 @@ namespace SimpleOccf {
 
         public void ModifyBranches(CstNode root) {
             foreach (var branch in FindBranchNodes(root)) {
-                branch.InsertCodeBeforeSelf("randoop.multi.OCCF.branch(" + BranchCount + ", ");
-                branch.InsertCodeAfterSelf(")");
-                BranchCount++;
+                if (branch.TokenText != "true") {
+                    branch.InsertCodeBeforeSelf("randoop.multi.OCCF.branch(" + BranchCount + ", ");
+                    branch.InsertCodeAfterSelf(")");
+                    BranchCount++;
+                }
             }
         }
 
@@ -47,7 +49,23 @@ namespace SimpleOccf {
         }
 
         private IEnumerable<CstNode> FindStatementNodes(CstNode root) {
-            return JavaElements.Statement(root);
+            return root.Descendants("statement")
+                    .Where(e => {
+                        // ブロック自身は意味を持たないステートメントで、中身だけが必要なので除外
+                        if (e.FirstChild.Name == "block") {
+                            return false;
+                        }
+                        // ラベルはループ文に付くため，ラベルの中身は除外
+                        var second = e.Parent.Children().ElementAtOrDefault(1);
+                        if (second != null && second.TokenText == ":"
+                            && e.Parent.Name == "statement") {
+                            return false;
+                        }
+                        if (e.FirstChild.TokenText == ";") {
+                            return false;
+                        }
+                        return true;
+                    });
         }
 
         private IEnumerable<CstNode> FindBranchNodes(CstNode root) {
