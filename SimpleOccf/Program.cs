8944afdc4504006ec6996ea026d0a5bfcaa3a534
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2014-06-16 18:06:26 +0900

    output source code metadata to *.soccf.xml

--- SimpleOccf/Program.cs
+++ SimpleOccf/Program.cs
@@ -1,11 +1,64 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using Code2Xml.Core.Generators;
+using Code2Xml.Core;
 
 namespace SimpleOccf {
     internal class Program {
         private const string BackupExtension = ".soccf_backup";
+        private const string MetadataExtension = ".soccf.xml";
+        private const string StatementIdAttributeName = "statement_id";
+        private const string BranchIdAttributeName = "branch_id";
+
+        private CstGenerator Generator;
+        private JavaCodeManipulator Manipulator;
+
+        public Program() {
+            Generator = CstGenerators.JavaUsingAntlr3;
+            Manipulator = new JavaCodeManipulator();
+        }
+
+        public void Instrument(string path) {
+            var backupPath = path + BackupExtension;
+            if (!File.Exists(backupPath)) {
+                File.Copy(path, backupPath);
+            }
+
+            var tree = Generator.GenerateTreeFromCodePath(backupPath);
+            Manipulator.SupplementBlock(tree);
+            Manipulator.ModifyStatements(tree);
+            Manipulator.ModifyBranches(tree);
+            // Check whether generated code is parasable or not
+            Generator.GenerateTreeFromCodeText(tree.Code, true);
+
+            File.WriteAllText(path, tree.Code);
+            var metadataPath = path + MetadataExtension;
+            File.WriteAllText(metadataPath, CstNodeToXml(tree).ToString());
+        }
+
+        private XElement CstNodeToXml(CstNode node) {
+            var element = new XElement(node.Name);
+            element.SetAttributeValue(Code2XmlConstants.IdAttributeName, node.RuleId);
+            if (Manipulator.StatementToId.ContainsKey(node)) {
+                element.SetAttributeValue(StatementIdAttributeName, Manipulator.StatementToId[node]);
+            }
+            if (Manipulator.BranchToId.ContainsKey(node)) {
+                element.SetAttributeValue(BranchIdAttributeName, Manipulator.BranchToId[node]);
+            }
+            if (node.Token == null) {
+                foreach (var child in node.Children()) {
+                    element.Add(CstNodeToXml(child));
+                }
+            } else {
+                foreach (var token in node.Hiddens) {
+                    element.Add(token.ToXmlFromHiddenToken());
+                }
+                element.Add(node.Token.ToXml());
+            }
+            return element;
+        }
 
         private static void Main(string[] args) {
             var paths = args.SelectMany(arg => {
@@ -18,29 +71,14 @@ namespace SimpleOccf {
                 }
             }).Select(Path.GetFullPath).Distinct();
 
-            var gen = CstGenerators.JavaUsingAntlr3;
-            var man = new JavaCodeManipulator();
-
+            var program = new Program();
             foreach (var path in paths) {
-                var backupPath = path + BackupExtension;
-                if (!File.Exists(backupPath)) {
-                    File.Copy(path, backupPath);
-                }
-                var tree = gen.GenerateTreeFromCodePath(backupPath);
-
-                man.SupplementBlock(tree);
-                man.ModifyStatements(tree);
-                man.ModifyBranches(tree);
-
-                // Check whether generated code is parasable or not
-                gen.GenerateTreeFromCodeText(tree.Code, true);
-
-                File.WriteAllText(path, tree.Code);
+                program.Instrument(path);
                 Console.Write(".");
             }
             Console.WriteLine();
-            Console.WriteLine("Statement: " + man.StatementCount);
-            Console.WriteLine("Branch: " + man.BranchCount);
+            Console.WriteLine("Statement: " + program.Manipulator.StatementCount);
+            Console.WriteLine("Branch: " + program.Manipulator.BranchCount);
         }
     }
 }
